a7d18e6868d1f71c8f6f6935b66a43ed7abfda1a
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-08-13 11:21:04 +0700

    deploy

--- src/main/java/com/phuongtd/book/BookApplication.java
+++ src/main/java/com/phuongtd/book/BookApplication.java
@@ -2,6 +2,7 @@ package com.phuongtd.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class BookApplication {
--- src/main/java/com/phuongtd/book/repositories/UserRepository.java
+++ src/main/java/com/phuongtd/book/repositories/UserRepository.java
@@ -2,10 +2,11 @@ package com.phuongtd.book.repositories;
 
 import com.phuongtd.book.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByEmail(String email);
 
